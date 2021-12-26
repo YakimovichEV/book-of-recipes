186c5701637ac4b46c29c33934573b90a03ba984
Eugene Yakimovich <yakimovichjv@gmail.com>
2021-12-26 21:26:54 +0200

    Set up `Next.js, Typescript, Tailwind ` on the project.

---
+++ pages/Home.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div className="font-primaryFont text-2xl text-red-500">Home page</div>
+    );
+};
+
+export default Home;
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./Home";
+
+const index = () => {
+    return (
+        <div className="font-primaryFont text-2xl text-red-500">
+            Hello world
+            <Home />
+        </div>
+    );
+};
+
+export default index;
