95b959343f1837e6167026b2218b083c48d92678
YakimovichEV <yakimovichjv@gmail.com>
2022-02-09 19:22:39 +0200

    Added new styles

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -7,16 +7,23 @@ const inputClasses = {
         "block w-4/5 h-12 bg-gray rounded-md p-3 mt-2 text-sm font-light border-none outline-none focus:ring-2 focus:ring-dijon",
     formSubmitInput:
         "w-1/2 bg-dijon text-white text-center rounded-xl h-12 mt-5 text-base cursor-pointer border-none outline-none duration-300 hover:bg-dark",
+    adminInput:
+        "w-full px-3 shadow-inner rounded-bl-sm rounded-tl-sm border border-gray300 focus:border-blue-700 focus:outline-none",
+    adminInputSecond:
+        "block py-2.5 px-0 w-full text-sm text-gray900 bg-transparent border-0 border-b-2 border-gray300 appearance-none dark:text-white dark:border-gray600 dark:focus:border-blue500 focus:outline-none focus:ring-0 focus:border-blue600 peer",
     label: "block mt-5 text-base font-medium text-white",
 } as const;
 
-interface IInputProps<T extends { [key: string]: any }> {
+type IInputProps<T extends { [key: string]: any }> = Omit<
+    React.HTMLProps<HTMLInputElement>,
+    "style"
+> & {
     style?: keyof typeof inputClasses;
     className?: string;
     label?: string;
-    type?: "text" | "password" | "tel" | "number";
+    type?: "text" | "password" | "tel" | "number" | "email";
     name: Path<T>;
-}
+};
 
 export const Input = <
     T extends { [key: string]: any } = { [key: string]: any },
@@ -26,6 +33,7 @@ export const Input = <
     style = "formInput",
     type = "text",
     className,
+    ...props
 }: IInputProps<T>) => {
     const {
         register,
@@ -43,6 +51,7 @@ export const Input = <
                 {...register(name)}
                 type={type}
                 className={classNames(stylesInput, className)}
+                {...props}
             />
             {errors[name] && errors[name].message && (
                 <div className="mt-2 text-red-500">{errors[name].message}</div>
