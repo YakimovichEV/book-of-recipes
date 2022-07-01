a16540401dc8df8bf7552d03898092be1ce8f07b
Alex Kislukhin <alexandr.kislukhin@gmail.com>
2022-07-01 11:33:02 +0100

    test image upload

--- server/resolvers/Recipe/mutations/createOrUpdateRecipe.ts
+++ server/resolvers/Recipe/mutations/createOrUpdateRecipe.ts
@@ -1,9 +1,12 @@
 import { MutationResolvers } from "../../../generated/graphql";
 import { Recipe } from "../../../entities/Recipe";
 import { UserInputError } from "apollo-server-micro";
+import { uploadFileS3 } from "../../../helpers/s3";
 
 export const createOrUpdateRecipe: MutationResolvers["createOrUpdateRecipe"] =
     async (_root, { recipe, recipeId }, { em }) => {
+        recipe.image = await uploadFileS3(recipe.image);
+
         if (recipeId) {
             await em.nativeUpdate(Recipe, { id: recipeId }, recipe);
 
